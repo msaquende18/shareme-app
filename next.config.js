1c00924ee8236e042628f45231b2ee1b501054cc
Marcelino Saquende <user@Users-MacBook-Pro.local>
2022-08-11 23:01:02 +0100

    sharameappv1

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+     domains: ['instagram.flad7-1.fna.fbcdn.net',
+     'lh3.googleusercontent.com',
+    ],
+  }
 }
 
 module.exports = nextConfig
